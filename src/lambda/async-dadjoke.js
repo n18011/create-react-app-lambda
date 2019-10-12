e7b35268121cd6e778a1bdee350cad1dd8bbf210
n18011 <n18011@std.it-college.ac.jp>
2019-10-12 18:36:23 +0900

    asdfghjkl

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -10,12 +10,20 @@ export async function handler (event, context) {
   const res = await client.tournaments.show({
     id: 'n18011test',
     callback: (err, data) => {
-      console.log(data)
-      return data
+      try {
+        console.log(data)
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ msg: res })
+
+        }
+      } catch (err) {
+        console.log(err) // output to netlify function log
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+        }
+      }
     }
   })
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ msg: res })
-  }
 }
