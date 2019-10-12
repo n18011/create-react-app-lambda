533ce5b764bacc3900742e9ddeb6843f161cabfc
n18011 <n18011@std.it-college.ac.jp>
2019-10-12 18:04:25 +0900

    asdf;la

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -9,16 +9,17 @@ const client = challonge.createClient({
 
 export async function handler (event, context) {
   try {
-    client.tournaments.show({
+    const res = await client.tournaments.show({
       id: 'n18011test',
       callback: (err, data) => {
         console.log(data)
-        return {
-          statusCode: 200,
-          body: JSON.stringify({ msg: data.tournament })
-        }
+        return data
       }
     })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ msg: res })
+    }
 
     /*
     const response = await axios.get('https://api.challonge.com/v1/tournaments.json', { headers: { Accept: 'application/json', api_key: 'THQwE1NobDxeWTRbAb8ACEtrUV4jDse7C6N7PwvU' } })
