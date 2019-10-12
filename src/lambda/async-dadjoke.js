3bf1f24ed6ef93540362d9b03400a6586159eec8
n18011 <n18011@std.it-college.ac.jp>
2019-10-12 17:58:09 +0900

    json change

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -15,7 +15,7 @@ export async function handler (event, context) {
         console.log(data)
         return {
           statusCode: 200,
-          body: JSON.stringify({ msg: data })
+          body: JSON.stringify({ msg: data.tournament })
         }
       }
     })
