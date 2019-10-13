9e8766389d5bada77b058aa495bf07dc045d84f8
n18011 <n18011@std.it-college.ac.jp>
2019-10-13 16:34:01 +0900

    asdfa

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -1,25 +1,17 @@
 // example of async handler using async-await
 // https://github.com/netlify/netlify-lambda/issues/43#issuecomment-444618311
 
-import axios from 'axios'
 const challonge = require('challonge')
 const client = challonge.createClient({
   apiKey: 'THQwE1NobDxeWTRbAb8ACEtrUV4jDse7C6N7PwvU'
 })
 
 export async function handler (event, context) {
-  try {
-    const response = await axios.get(`https://n18011:${process.env.APIKEY}@api.challonge.com/v1/tournaments.json`, { headers: { Accept: 'application/json' } })
-    const data = response.data
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data)
+  client.tournaments.show({
+    id: 'n18011',
+    callback: (err, data) => {
+      console.log(err, data)
+      return data
     }
-  } catch (err) {
-    console.log(err) // output to netlify function log
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
-    }
-  }
+  })
 }
