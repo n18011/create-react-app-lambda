da81b0172ece4b8d33026d1268d4ed268cbac89e
n18011 <n18011@std.it-college.ac.jp>
2019-10-15 10:11:43 +0900

    10/15 g

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -2,22 +2,31 @@
 // https://github.com/netlify/netlify-lambda/issues/43#issuecomment-444618311
 
 const challonge = require('challonge')
+const request = require('superagent')
 const client = challonge.createClient({
   apiKey: 'THQwE1NobDxeWTRbAb8ACEtrUV4jDse7C6N7PwvU'
 })
 
-export async function handler (event, context) {
+/*
+const data = () => {
+  request.get('https://n18011:THQwE1NobDxeWTRbAb8ACEtrUV4jDse7C6N7PwvU@api.challonge.com/v1/tournaments.json')
+  .end((err, res) => {
+    console.log(res)
+  })
+}
+*/
+
+export async function handler (event, context, callback) {
+  /*
   const data = () => {
-    client.tournaments.show({
-      id: 'n18011',
-      callback: (err, data) => {
-        console.log(err, data)
-        return data
-      }
-    })
+    request.get('https://n18011:THQwE1NobDxeWTRbAb8ACEtrUV4jDse7C6N7PwvU@api.challonge.com/v1/tournaments.json')
+      .end((err, res) => {
+        console.log(res)
+      })
   }
-  return {
+  */
+  callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: data() })
-  }
+    body: JSON.stringify({ msg: 'Hello, World!' })
+  })
 }
