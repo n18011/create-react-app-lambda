174ca3aec985e002427107c2961ac7bf4fb4171a
n18011 <n18011@std.it-college.ac.jp>
2019-10-15 10:16:47 +0900

    10/15 h

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -17,16 +17,14 @@ const data = () => {
 */
 
 export async function handler (event, context, callback) {
-  /*
   const data = () => {
     request.get('https://n18011:THQwE1NobDxeWTRbAb8ACEtrUV4jDse7C6N7PwvU@api.challonge.com/v1/tournaments.json')
       .end((err, res) => {
         console.log(res)
       })
   }
-  */
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!' })
+    body: JSON.stringify({ msg: await data() })
   })
 }
